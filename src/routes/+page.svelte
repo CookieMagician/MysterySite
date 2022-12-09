ae141b01f073be43f4c45effbb8336c4bbe90330
Kristoffer Zaar <kristofferzaard@gmail.com>
2022-12-09 11:46:43 +0100

    initial commit 2

---
+++ src/routes/+page.svelte
@@ -0,0 +1,266 @@
+<script>
+    import Question from "./question.svelte";
+    let loggedIn = false;
+    let username;
+    let password;
+    let q = 1;
+
+    let correctUsername = "1";
+    let correctPassword = "1";
+
+    function checkLogin() {
+		if (username === correctUsername && password === correctPassword) {
+            loggedIn = true;
+            console.log("passwords were right")
+        }
+        
+	}
+
+    function checkAnswer(a, b) {
+        if (a === b) {
+            q++;
+        }
+    }
+</script>
+
+<div class="crt">
+    <img src="scorp.png"/>
+    {#if q < 6}
+        {#if !loggedIn}
+        <div>
+        <h1>Log in:</h1>
+            <input placeholder="Username" bind:value={username}>
+            <input placeholder="Password" bind:value={password}>
+            <button on:click={checkLogin}>Login</button>
+            <p class="error">That is not right!</p>
+        </div>
+        {/if}
+
+        {#if loggedIn}
+        <h1>Välkommen {correctUsername}!</h1>
+        <p>Svara på våra fem sista frågor innan du kan gå vidare.</p>
+        <br/>
+            {#if q === 1}
+            <Question number=1 question="What do you think about the current political situation in azerbadjan?" answer="1" check={checkAnswer} ></Question>
+            {/if}
+            {#if q === 2}
+            <Question number=2 question="Do you like me" answer="1"check={checkAnswer}></Question>
+            {/if}
+            {#if q === 3}
+            <Question number=3 question="Wroom Wroom bitch" answer="1"check={checkAnswer}></Question>
+            {/if}
+            {#if q === 4}
+            <Question number=4 question="I  love cheese!?" answer="1"check={checkAnswer}></Question>
+            {/if}
+            {#if q === 5}
+            <Question number=5 question="JJJ" answer="1"check={checkAnswer}></Question>
+            {/if}
+            
+    
+        {/if}
+    
+    
+    {:else}
+        <h1>Vi ses!</h1>
+        <h2>Scorpiongränd 22</h2>
+    {/if}
+
+    
+</div>
+
+<style>
+    .error {
+        color: red;
+    }
+    
+    img {
+        position:absolute;
+        width: 100%;
+        height: 100%;
+        top: 0px;
+        left: 0px;
+        pointer-events: none;
+        opacity: 50%;
+        z-index: -1;
+    }
+    h1 {
+        color: greenyellow;
+    }
+
+    input {
+        background-color: rgb(15, 15, 15);
+        border-color: greenyellow;
+        color: greenyellow;
+    }
+    button {
+        background-color: rgb(15, 15, 15);
+        color: greenyellow;
+        border-color: greenyellow;
+    }
+    div {
+        text-align: center;
+    
+        font-family: VT323;
+    }
+    @keyframes flicker {
+  0% {
+    opacity: 0.27861;
+  }
+  5% {
+    opacity: 0.34769;
+  }
+  10% {
+    opacity: 0.23604;
+  }
+  15% {
+    opacity: 0.90626;
+  }
+  20% {
+    opacity: 0.18128;
+  }
+  25% {
+    opacity: 0.83891;
+  }
+  30% {
+    opacity: 0.65583;
+  }
+  35% {
+    opacity: 0.67807;
+  }
+  40% {
+    opacity: 0.26559;
+  }
+  45% {
+    opacity: 0.84693;
+  }
+  50% {
+    opacity: 0.96019;
+  }
+  55% {
+    opacity: 0.08594;
+  }
+  60% {
+    opacity: 0.20313;
+  }
+  65% {
+    opacity: 0.71988;
+  }
+  70% {
+    opacity: 0.53455;
+  }
+  75% {
+    opacity: 0.37288;
+  }
+  80% {
+    opacity: 0.71428;
+  }
+  85% {
+    opacity: 0.70419;
+  }
+  90% {
+    opacity: 0.7003;
+  }
+  95% {
+    opacity: 0.36108;
+  }
+  100% {
+    opacity: 0.24387;
+  }
+}
+@keyframes textShadow {
+  0% {
+    text-shadow: 0.4389924193300864px 0 1px rgba(0,30,255,0.5), -0.4389924193300864px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  5% {
+    text-shadow: 2.7928974010788217px 0 1px rgba(0,30,255,0.5), -2.7928974010788217px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  10% {
+    text-shadow: 0.02956275843481219px 0 1px rgba(0,30,255,0.5), -0.02956275843481219px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  15% {
+    text-shadow: 0.40218538552878136px 0 1px rgba(0,30,255,0.5), -0.40218538552878136px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  20% {
+    text-shadow: 3.4794037899852017px 0 1px rgba(0,30,255,0.5), -3.4794037899852017px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  25% {
+    text-shadow: 1.6125630401149584px 0 1px rgba(0,30,255,0.5), -1.6125630401149584px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  30% {
+    text-shadow: 0.7015590085143956px 0 1px rgba(0,30,255,0.5), -0.7015590085143956px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  35% {
+    text-shadow: 3.896914047650351px 0 1px rgba(0,30,255,0.5), -3.896914047650351px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  40% {
+    text-shadow: 3.870905614848819px 0 1px rgba(0,30,255,0.5), -3.870905614848819px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  45% {
+    text-shadow: 2.231056963361899px 0 1px rgba(0,30,255,0.5), -2.231056963361899px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  50% {
+    text-shadow: 0.08084290417898504px 0 1px rgba(0,30,255,0.5), -0.08084290417898504px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  55% {
+    text-shadow: 2.3758461067427543px 0 1px rgba(0,30,255,0.5), -2.3758461067427543px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  60% {
+    text-shadow: 2.202193051050636px 0 1px rgba(0,30,255,0.5), -2.202193051050636px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  65% {
+    text-shadow: 2.8638780614874975px 0 1px rgba(0,30,255,0.5), -2.8638780614874975px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  70% {
+    text-shadow: 0.48874025155497314px 0 1px rgba(0,30,255,0.5), -0.48874025155497314px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  75% {
+    text-shadow: 1.8948491305757957px 0 1px rgba(0,30,255,0.5), -1.8948491305757957px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  80% {
+    text-shadow: 0.0833037308038857px 0 1px rgba(0,30,255,0.5), -0.0833037308038857px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  85% {
+    text-shadow: 0.09769827255241735px 0 1px rgba(0,30,255,0.5), -0.09769827255241735px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  90% {
+    text-shadow: 3.443339761481782px 0 1px rgba(0,30,255,0.5), -3.443339761481782px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  95% {
+    text-shadow: 2.1841838852799786px 0 1px rgba(0,30,255,0.5), -2.1841838852799786px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+  100% {
+    text-shadow: 2.6208764473832513px 0 1px rgba(0,30,255,0.5), -2.6208764473832513px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+}
+.crt::after {
+  content: " ";
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(18, 16, 16, 0.1);
+  opacity: 0;
+  z-index: 2;
+  pointer-events: none;
+  animation: flicker 3.5s infinite;
+}
+.crt::before {
+  content: " ";
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: linear-gradient(rgba(18, 16, 16, 0) 50%, rgba(0, 0, 0, 0.25) 50%), linear-gradient(90deg, rgba(255, 0, 0, 0.06), rgba(0, 255, 0, 0.02), rgba(0, 0, 255, 0.06));
+  z-index: 2;
+  background-size: 100% 2px, 3px 100%;
+  pointer-events: none;
+}
+.crt {
+  animation: textShadow 2.6s infinite;
+}
+
+</style>
---
+++ src/routes/question.svelte
@@ -0,0 +1,47 @@
+<script>
+    export let number;
+    export let question;
+    export let answer;
+    export let check;
+    let usrAnswer;
+
+</script>
+
+<div>
+    <h3>Question {number}:</h3>
+    <h3>{question}</h3>
+    <input placeholder="Svar" bind:value={usrAnswer}>
+
+    <button on:click={() => check(answer, usrAnswer)}>Check answer</button>
+    <p class="error">That is not right!</p>
+</div>
+
+<style>
+    .error {
+        color: red;
+    }
+    img {
+        position:absolute;
+        width: 100%;
+        height: 100%;
+        top: 0px;
+        left: 0px;
+        pointer-events: none;
+        opacity: 50%;
+        z-index: -1;
+    }
+    h1 {
+        color: greenyellow;
+    }
+
+    input {
+        background-color: rgb(15, 15, 15);
+        border-color: greenyellow;
+        color: greenyellow;
+    }
+    button {
+        background-color: rgb(15, 15, 15);
+        color: greenyellow;
+        border-color: greenyellow;
+    }
+</style>
